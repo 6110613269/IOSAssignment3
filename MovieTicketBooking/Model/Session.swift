663109e2fa3c454542c5e702fa5b1f76b06aaa42
Pichamon Visessan <130810372+jinxxy69@users.noreply.github.com>
2024-05-15 08:52:44 +1000

    Update Session.swift

--- MovieTicketBooking/Model/Session.swift
+++ MovieTicketBooking/Model/Session.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// Represents a session or event, conforming to Codable for encoding/decoding
+// Contains properties for date, id (formatted as "dd/MM/yyyy HH:mm"), month, day, and time.
 struct Session: Codable {
     var date: Date
     var id: String { date.toString("dd/MM/yyyy HH:mm") }
@@ -17,7 +19,7 @@ struct Session: Codable {
         get { date.time() }
         set {
             if let newDate = DateFormatter.format("dd/MM/yyyy HH:mm", "\(date.toString()) \(newValue)") {
-            date = newDate
+                date = newDate
             }
         }
     }
@@ -25,17 +27,19 @@ struct Session: Codable {
     func isIdentical(_ session: Session?) -> Bool {
         guard let session = session else { return false }
         return id == session.id
-        }
+    }
     
+    // Initializes a Session instance with provided date.
     init(date: Date) {
         self.date = date
     }
 }
 
-    private extension DateFormatter {
-        static func format(_ format: String, _ dateString: String) -> Date? {
-            let formatter = DateFormatter()
-            formatter.dateFormat = format
-            return formatter.date(from: dateString)
-        }
+// Provides a static method to format a string to a Date object using a specified format
+private extension DateFormatter {
+    static func format(_ format: String, _ dateString: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        return formatter.date(from: dateString)
+    }
 }
