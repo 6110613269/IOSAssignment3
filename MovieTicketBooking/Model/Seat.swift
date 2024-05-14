941dca9eb50acff58eb489b147fa328a844e6e0e
Pichamon Visessan <130810372+jinxxy69@users.noreply.github.com>
2024-05-15 08:48:43 +1000

    Update Seat.swift

--- MovieTicketBooking/Model/Seat.swift
+++ MovieTicketBooking/Model/Seat.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+// Contains properties for section, row, selected (whether the seat is selected by the user, sold (whether the seat is sold), and a computed property seatCode representing the unique code of the seat.
 struct Seat: Codable {
     var section: Int
     var row: Int
@@ -14,8 +15,10 @@ struct Seat: Codable {
     var sold: Bool
     var seatCode: String { get { "\(["A", "B", "C", "D", "E", "F", "G", "H"][section])\(row + 1)" } }
     
+    // Checks if two Seat instances are identical based on their seatCode.
     func isIdentical(_ otherSeat: Seat) -> Bool { seatCode == otherSeat.seatCode }
 
+    // Parameters include section, row, selected (defaulted to false), and sold (defaulted to false)
     init(_ section: Int, _ row: Int, selected: Bool = false, sold: Bool = false) {
         self.section = section
         self.row = row
