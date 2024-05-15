89faf4e0bdffdcafc5a6c628b1b92ffd94e23027
shalalunlunlunlunla <sblaq-absolute@hotmail.com>
2024-05-15 12:03:15 +1000

    Update SeatsViewController.swift

--- MovieTicketBooking/View/SeatsViewController.swift
+++ MovieTicketBooking/View/SeatsViewController.swift
@@ -33,7 +33,8 @@ final class SeatsViewController: UIViewController {
     }
     
     // MARK: - Functions
-    
+
+    // Create seats for the cinema layout.
     private func createSeats() {
         guard let movie = movie, let session = session else { return }
         let soldSeats: [Seat] = Ticket.soldSeats(movie: movie, session: session)
@@ -47,27 +48,33 @@ final class SeatsViewController: UIViewController {
             }
         }
     }
-    
+
+    // Filter selected seats from all seats.
     private func selectedSeats() -> [Seat] { seats.filter { $0.selected } }
-    
+
+    // Calculate the number of selected seats.
     private func selectedSeatsCount() -> Int { selectedSeats().count }
-    
+
+    // Generate seat code representing selected seats.
     private func selectedSeatsCodes() -> String {
         let selectedSeatCodes = selectedSeats().map { seat in seat.seatCode }
         return selectedSeatCodes.joined(separator: ", ")
     }
-    
+
+    // Calculate the total price for selected seats based on a fixed price of $20 per seat.
     private func calculateTotalPrice() -> Double { Double(selectedSeatsCount()) * 20.0 }
         
     private func seatIndexOf(_ indexPath: IndexPath) -> Int {
         return indexPath.section * 9 + indexPath.row
     }
-    
+
+    // Handle seat selection and deselection.
     private func seatOnTap(_ index: Int) {
         guard seats[index].sold != true else { return }
         let isSelected = seats[index].selected
+        // Check if the selection limit is reached and toggle seat selection.
         guard selectedSeatsCount() < 10 || isSelected else {
-            self.showToast("You can not buy more than 10 tickets")
+            self.showToast("You can not buy more than 10 tickets") 
             return
         }
         seats[index].selected = !isSelected
@@ -78,12 +85,15 @@ final class SeatsViewController: UIViewController {
     }
     
     // MARK: - Actions
-    
+
+    // Action triggered by pressing the "Buy Tickets" button.
     @IBAction private func btnBuyTickets_TUI(_ sender: Any) {
+        // Check if any seats have been selected.
         guard selectedSeatsCount() > 0 else {
             self.showToast("Please choose at least 1 seat")
             return
         }
+        // Create the ticket and navigate to confirmation page.
         let ticketVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "TicketVC") as! TicketViewController
         let ticket = Ticket(movie: movie!, session: session!, seats: selectedSeats(), price: calculateTotalPrice())
         ticketVC.ticket = ticket
