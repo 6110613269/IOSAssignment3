3865fd8f6bd5f0fb410c6a9a895bb5cb5daf16a4
Pichamon Visessan <130810372+jinxxy69@users.noreply.github.com>
2024-05-17 09:45:04 +1000

    Update UpcomingMoviePosterCVC.swift

--- MovieTicketBooking/Nib/CollectionViewCell/UpcomingMoviePoster/UpcomingMoviePosterCVC.swift
+++ MovieTicketBooking/Nib/CollectionViewCell/UpcomingMoviePoster/UpcomingMoviePosterCVC.swift
@@ -8,29 +8,31 @@
 import UIKit
 
 class UpcomingMoviePosterCVC: UICollectionViewCell {
-  
-  // MARK: - Outlets
-  @IBOutlet weak private var imageIV: UIImageView!
-  @IBOutlet weak private var titleLabel: UILabel!
-  @IBOutlet weak private var screeningDatesLabel: UILabel!
-  
-  // MARK: - Functions
-  func setupCell(_ movie: Movie) {
-    imageIV.image = movie.imageName.loadImageString()
-    titleLabel.text = movie.title
     
-    // Format screening dates into a string
-    let screeningDatesString = formatScreeningDates(movie.screeningDate)
-    screeningDatesLabel.text = screeningDatesString
-  }
-  
-  private func formatScreeningDates(_ screeningDates: [Date]) -> String {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MMM dd, yyyy"  // Customize the date format as needed
-    let dateStringArray = screeningDates.map { formatter.string(from: $0) }
-    let datesString = dateStringArray.joined(separator: ", ")
-    return "\(datesString)"
-  }
-  
+    // MARK: - Outlets
+    @IBOutlet weak private var imageIV: UIImageView!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var screeningDatesLabel: UILabel!
+    
+    // MARK: - Functions
+    
+    // Configures the cell with movie data
+    func setupCell(_ movie: Movie) {
+        imageIV.image = movie.imageName.loadImageString()
+        titleLabel.text = movie.title
+        
+        // Format screening dates into a string
+        let screeningDatesString = formatScreeningDates(movie.screeningDate)
+        screeningDatesLabel.text = screeningDatesString
+    }
+    
+    // Sets the screening dates for the movie
+    private func formatScreeningDates(_ screeningDates: [Date]) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, yyyy"  // Customize the date format
+        let dateStringArray = screeningDates.map { formatter.string(from: $0) }
+        let datesString = dateStringArray.joined(separator: ", ")
+        return "\(datesString)"
+    }
 }
 
