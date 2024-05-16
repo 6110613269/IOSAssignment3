fd984cdd36c7f6f4b78ec063922da7c26a571f09
Pichamon Visessan <130810372+jinxxy69@users.noreply.github.com>
2024-05-17 08:30:31 +1000

    Update MovieCVC.swift

--- MovieTicketBooking/Nib/CollectionViewCell/Movie/MovieCVC.swift
+++ MovieTicketBooking/Nib/CollectionViewCell/Movie/MovieCVC.swift
@@ -8,28 +8,26 @@
 import UIKit
 
 class MovieCVC: UICollectionViewCell {
-  
-  // MARK: - Outlets
-  
-  @IBOutlet weak private var imageIV: UIImageView!
-  @IBOutlet weak private var titleLabel: UILabel!
-  @IBOutlet weak private var screeningDatesLabel: UILabel!
-  
-  // MARK: - Functions
-  
-  func setupCell(_ movie: Movie) {
-    imageIV.image = movie.imageName.loadImageString()
-    titleLabel.text = movie.title
-    setScreeningDates(movie)
-  }
-  
-  // Function to set screening dates
-  private func setScreeningDates(_ movie: Movie) {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "MMM dd, yyyy"  // Customize the date format as needed
     
-    let screeningDatesString = movie.screeningDate.map { formatter.string(from: $0) }.joined(separator: ", ")
-    screeningDatesLabel.text = "\(screeningDatesString)"
-  }
+    // MARK: - Outlets
+    @IBOutlet weak private var imageIV: UIImageView!
+    @IBOutlet weak private var titleLabel: UILabel!
+    @IBOutlet weak private var screeningDatesLabel: UILabel!
+    
+    // MARK: - Functions
+    // Configures the cell with movie data
+    func setupCell(_ movie: Movie) {
+        imageIV.image = movie.imageName.loadImageString()
+        titleLabel.text = movie.title
+        setScreeningDates(movie)
+    }
+    
+    // Sets the screening dates for the movie
+    private func setScreeningDates(_ movie: Movie) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM dd, yyyy"  // Customize the date format
+        let screeningDatesString = movie.screeningDate.map { formatter.string(from: $0)
+        }.joined(separator: ", ")
+        screeningDatesLabel.text = "\(screeningDatesString)"
+    }
 }
-
